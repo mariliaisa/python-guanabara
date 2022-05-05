9e48778f7621ac5bd09f3dbdc75a500e6e8dcbe0
Marília Galvão <mariliaisabele@gmail.com>
2022-05-05 14:39:34 +0100

    exclusão de arquivos de teste

--- Aula13.Desafio052.victor.py
+++
@@ -1,8 +0,0 @@
-num = int(input('número: '))
-teste = 'é primo'
-if num != 1:
-    for i in range (2,num):
-        if num % i == 0:
-            teste = 'Não é primo'
-print('O número {} {}'.format(num, teste))
-
